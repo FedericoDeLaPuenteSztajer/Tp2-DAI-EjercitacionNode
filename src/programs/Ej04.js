61951519073f375ab04c93e2ec9f329900c855c6
49319314@est.ort.edu.ar <devandroid@ort>
2026-03-30 09:41:58 -0300

    Ej04 incompleto, continuar luego

--- src/programs/Ej04.js
+++ src/programs/Ej04.js
@@ -1,14 +1,32 @@
-import fs from 'fs'
-let escrito= '../textfiles/CuentoGenial.txt'
+/**import fs from 'fs'
+let escrito= '../textfiles/CuentoMatiasGerez'
 
 function RobarTexto(file){
 
-    const content= fs.readFile(file, 'utf-8')
+    const content= fs.readFile(file, 'utf-8')    
     console.log(content)
 
-    fs.rename(file, "../textfiles/CuentoGustaboBuenotte.txt")
+    fs.rename(file, "../textfiles/CuentoGustaboBuenotte")
     console.log("Archivo robado")
 
 }
 
 RobarTexto(escrito)
+*/
+
+//-----------------------------------------------------------------------
+
+/**
+import fs from 'fs/pormises';
+
+const ARCHIVO_ENTRADA           = "../textfiles/CuentoMatiasGerez";
+const ARCHIVO_SALIDA            = "../textfiles/CuentoGustaboBuenotte";
+
+function copiar(origen, destino){
+
+    fs.readFile(origen, utf-8)
+
+}
+
+copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
+*/
