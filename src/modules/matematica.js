fdeba69ba6447115e3c82bbd6398d92cd65ed4e4
49319314@est.ort.edu.ar <devandroid@ort>
2026-03-30 08:26:48 -0300

    Ej02 + module matematica

---
+++ src/modules/matematica.js
@@ -0,0 +1,14 @@
+const PI= Math.PI
+
+const Sumar= (a,b) => a+b;
+const Restar= (a,b) => a-b;
+
+function Multiplicar3 (a,b,c){
+    return  a*b*c;
+}
+
+function Promedio3 (a,b,c){
+    return (a+b+c)/3;
+}
+
+export {PI, Sumar, Restar, Multiplicar3, Promedio3};
---
+++ src/programs/Ej02.js
@@ -0,0 +1,7 @@
+import {PI, Sumar, Restar, Multiplicar3, Promedio3} from '../modules/matematica.js'
+
+console.log(`PI: ${PI}`)
+console.log(`Suma: ${Sumar(1,2)}`)
+console.log(`Restar: ${Restar(1,2)}`)
+console.log(`Multiplicar: ${Multiplicar3(1,2,3)}`)
+console.log(`Promedio: ${Promedio3(1,2,3)}`)
